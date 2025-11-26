0bb3637c051accb93460541e8711aa3d707f6fa0
Hafza Abdullahi <C00286249@setu.ie>
2025-11-26 03:03:49 +0000

    solved dependency issues with emulators and flutter sdk, homescreen code refactored slightly

---
+++ Flutter/App/myapp/lib/src/constants/text_strings.dart
@@ -0,0 +1,24 @@
+/* --- ALL TEXT FOR APP -- */
+
+/* -- Normal Text -- */
+const String tLogin = "Login";
+const String tRegister = "Register";
+
+/* -- Welcome Screen text */
+const String tWelcomeTitle = "Welcome to TAP 2 SHOP";
+const String tWelcomeSubTitle =
+    "The go-to App for supporting your local school";
+
+/*---- Login Screen Text ----- */
+const String tLoginTitle = "Welcome Back!";
+const String tEmail = "Email";
+const String tPassword = "Password";
+const String tForgotPassword = "Forgot Password?";
+
+
+
+const String tSignInWithGoogle = "Sign-in with Google";
+const String tAlreadyHaveAccount = "Already have an account?";
+
+
+
