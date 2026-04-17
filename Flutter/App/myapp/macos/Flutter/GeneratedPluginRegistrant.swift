8df406758bf60c4b2f7887171c55ba32ca705bdf
Hafza Abdullahi <C00286249@setu.ie>
2026-04-17 22:18:28 +0100

    indention problems and library fix

--- Flutter/App/myapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/App/myapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import file_picker
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
-import path_provider_foundation
 import record_macos
 import url_launcher_macos
 
@@ -24,7 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
